379547fafc19fa387503908c9e9533a2da051229
vedprakash25 <ved1998karma@gmail.com>
2024-02-26 18:48:34 +0530

    push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ export { default as AnimatedText } from "./animatedText";
 export { default as ToDoApp } from "./toDoApp";
 export { default as HomePage } from "./home";
 export { default as Parallax } from "./parallax";
-export { default as BlogsDetails } from "./home/blogsDetails";
+// export { default as BlogsDetails } from "./home/blogsDetails";
 export { default as API } from "./api";
